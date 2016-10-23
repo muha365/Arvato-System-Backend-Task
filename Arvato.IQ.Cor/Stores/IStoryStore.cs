ff43b749eb61d870e3e81912c91f7fc7ee5a5626
Muha <Muhammad@DEBUG365>
2016-10-24 05:29:17 +0800

    Adding 2 interfaces IStoryStore the first that exposes story store basic api with generic key type. The second exposes story store basic api with key type of int64

---
+++ Arvato.IQ.Cor/Stores/IStoryStore.cs
@@ -0,0 +1,31 @@
+﻿using Arvato.IQ.Cor.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Cor.Stores
+{
+    /// <summary>
+    /// interface that exposes story store basic api with key type of int64.  
+    /// </summary>
+    /// <typeparam name="TStory"></typeparam>
+    public interface IStoryStore<TStory> : IStoryStore<TStory, long>, IStore<TStory, long>
+        where TStory : class, IStory<long>, new()
+    {
+
+    }
+
+    /// <summary>
+    /// interface that exposes story store basic api with generic key type.  
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IStoryStore<TEntity, in TKey> : IStore<TEntity, TKey>
+       where TEntity : class, IStory<TKey>, new()
+        where TKey : IEquatable<TKey>
+    {
+
+    }
+}
