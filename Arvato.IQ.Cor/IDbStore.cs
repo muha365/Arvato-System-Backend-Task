79e6193937e82a011d502af614fd0fdc2314ece1
Muha <Muhammad@DEBUG365>
2016-10-24 05:09:10 +0800

    Adding IDbStore contract that works as unit of work for the database implementation and exposes basic Database api

---
+++ Arvato.IQ.Cor/IDbStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Cor
+{
+    /// <summary>
+    /// interface that exposes basic database unit of work api
+    /// </summary>
+    public interface IDbStore : IDisposable
+    {
+
+        /// <summary>
+        /// Save changes Asynchronously 
+        /// </summary>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync();
+
+        /// <summary>
+        /// Save changes Synchronously
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+    }
+}
