876417fbab3507893a20eb1adff10df6ba3a111f
Muha <Muhammad@DEBUG365>
2016-10-24 07:59:44 +0800

    Adding DbStore Configuration class for better DbContext to database talks and intialization

---
+++ Arvato.IQ.Data/DbStoreConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Data
+{
+    /// <summary>
+    /// DbStore Configuration 
+    /// </summary>
+    public class DbStoreConfig : DbConfiguration
+    {
+        /// <summary>
+        /// the default constructor
+        /// </summary>
+        public DbStoreConfig()
+        {
+            SetDatabaseInitializer<DbStore>(null);
+            SetManifestTokenResolver(new DefaultManifestTokenResolver());
+        }
+
+        internal class DefaultManifestTokenResolver : IManifestTokenResolver
+        {
+            private readonly IManifestTokenResolver _defaultResolver = new System.Data.Entity.Infrastructure.DefaultManifestTokenResolver();
+
+            public string ResolveManifestToken(DbConnection connection)
+            {
+                var sqlConn = connection as SqlConnection;
+                if (sqlConn != null && sqlConn.DataSource == @".")
+                {
+                    return "2012";
+                }
+                else
+                {
+                    return _defaultResolver.ResolveManifestToken(connection);
+                }
+            }
+        }
+
+    }
+}
