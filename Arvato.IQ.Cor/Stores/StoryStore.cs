e54079a54a7fb0310970b001934ee306843f6700
Muha <Muhammad@DEBUG365>
2016-10-24 05:21:46 +0800

    Adding IStoryStore contracts that inherit from basic IStore and exposes basic IStory store api funtions

---
+++ Arvato.IQ.Cor/Stores/StoryStore.cs
@@ -0,0 +1,28 @@
+﻿using Arvato.IQ.Cor.Entities;
+using System;
+
+namespace Arvato.IQ.Cor.Stores
+{
+    /// <summary>
+    /// interface that exposes and represent basic story store api with default story key as int64
+    /// </summary>
+    /// <typeparam name="TStory"></typeparam>
+    public interface IStoryStore<TStory> : IStoryStore<TStory, long>, IStore<TStory, long>
+            where TStory : class, IStory<long>, new()
+        {
+
+        }
+
+    /// <summary>
+    /// interface that exposes and represent basic story store api
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IStoryStore<TEntity, in TKey> : IStore<TEntity, TKey>
+           where TEntity : class, IStory<TKey>, new()
+            where TKey : IEquatable<TKey>
+        {
+
+        }
+    }
+}
