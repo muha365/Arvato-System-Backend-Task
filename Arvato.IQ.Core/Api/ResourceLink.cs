14c1338f8db103422868aaada000f32a8f3cdcf6
Muha <Muhammad@DEBUG365>
2016-10-24 06:47:04 +0800

    Adding ResourceLink that represent resource link adding text project and test resource link class

---
+++ Arvato.IQ.Core/Api/ResourceLink.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Core.Api
+{
+
+    /// <summary>
+    /// class represent resource link 
+    /// </summary>
+    public class ResourceLink
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ResourceLink()
+        {
+        }
+
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        /// <param name="rel">the relation between the resource and href</param>
+        /// <param name="href">hypertext reference</param>
+        /// <param name="method">the http method </param>
+        public ResourceLink(string rel, string href, string method)
+        {
+            //validate rel parameter
+            if (rel == null)
+            {
+                throw new ArgumentNullException( nameof(rel),"{0} parameter can't be null");
+            }
+            if (string.Empty == rel)
+            {
+                throw new ArgumentException("{0} parameter can't be empty string", nameof(rel));
+            }
+            this.Rel = rel;
+
+            // validate url parameter
+            if (href == null)
+            {
+                throw new ArgumentNullException(nameof(href),"{0} parameter can't be null");
+            }
+            if (string.Empty == href)
+            {
+                throw new ArgumentException("{0} parameter can't be empty string", nameof(href));
+            }
+            this.Href = href;
+
+            // validate method parameter
+            if (method == null)
+            {
+                throw new ArgumentNullException(nameof(method), "{0} parameter can't be null");
+            }
+            if (string.Empty == method)
+            {
+                throw new ArgumentException("{0} parameter can't be empty string", nameof(method));
+            }
+            this.Method = method;
+        }
+
+        /// <summary>
+        /// Relation between the resource and href
+        /// </summary>
+        public string Rel { get; set; }
+
+        /// <summary>
+        /// Hypertext reference
+        /// </summary>
+        public string Href { get; set; }
+
+        /// <summary>
+        /// Http method 
+        /// </summary>
+        public String Method { get; set; }
+    }
+}
---
+++ Tests/Arvato.IQ.Tests/Core/ResourceLinkTests.cs
@@ -0,0 +1,57 @@
+﻿using Arvato.IQ.Core.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Arvato.IQ.Tests.Core
+{
+    public class ResourceLinkTests
+    {
+
+        [Fact]
+        public void Should_Throw_ArgumentException_When_Rel_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentException>(() => new ResourceLink(string.Empty,"/resource/1","GET"));
+            Assert.Equal(ex.ParamName, "rel", ignoreCase: true);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentNullException_When_Rel_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentNullException>(() => new ResourceLink(null,"/resource/1","GET"));
+            Assert.Equal(ex.ParamName, "rel", ignoreCase: true);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentException_When_Href_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentException>(() => new ResourceLink("create",string.Empty,"GET"));
+            Assert.Equal(ex.ParamName, "href", ignoreCase: true);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentNullException_When_Href_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentNullException>(() => new ResourceLink("create",null,"GET"));
+            Assert.Equal(ex.ParamName, "href", ignoreCase: true);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentException_When_Method_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentException>(() => new ResourceLink("create","/resource/1",string.Empty));
+            Assert.Equal(ex.ParamName, "method", ignoreCase: true);
+        }
+
+        [Fact]
+        public void Should_Throw_ArgumentNullException_When_Method_Empty()
+        {
+            var ex =  Assert.Throws<ArgumentNullException>(() => new ResourceLink("create","/resource/1",null));
+            Assert.Equal(ex.ParamName, "method", ignoreCase: true);
+        }
+
+    }
+}
