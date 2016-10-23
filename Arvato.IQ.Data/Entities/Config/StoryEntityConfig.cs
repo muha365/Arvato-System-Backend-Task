9c39c2837df53e489943f48874dc152b6c3bab64
Muha <Muhammad@DEBUG365>
2016-10-24 07:30:50 +0800

    Adding story default entity framework implementation and its configuration class

---
+++ Arvato.IQ.Data/Entities/Config/StoryEntityConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Data.Entities.Config
+{
+    internal class StoryEntityConfig : EntityTypeConfiguration<Story>
+    {
+        public StoryEntityConfig()
+        {
+            this.HasKey(t => t.StoryId);
+            this.Property(t => t.Title)
+                .HasMaxLength(500)
+                .IsRequired();
+            
+        }
+    }
+}
---
+++ Arvato.IQ.Data/Entities/Story.cs
@@ -0,0 +1,55 @@
+﻿using Arvato.IQ.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Data
+{ 
+
+    /// <summary>
+    /// Default entityframework implementation for story with the key of type Int64
+    /// </summary>
+    public class Story : Story<long>
+    {
+    }
+
+    /// <summary>
+    /// Default entityframework implementation for story 
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    public class Story<TKey> : IStory<TKey>
+        where TKey : IEquatable<TKey>
+    {
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Story()
+        {
+        }
+
+        /// <summary>
+        /// story Id
+        /// </summary>
+        public TKey StoryId { get; set; }
+
+        /// <summary>
+        /// story title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// story description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// published at
+        /// </summary>
+        public DateTime PublishedAt { get; set; }
+
+    }
+
+}
