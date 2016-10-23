899cb1304a3bca8f07d6637e0f6469df28de8b71
Muha <Muhammad@DEBUG365>
2016-10-24 07:42:23 +0800

    Adding DbStore as default implementation of IDbSTore and as Data DbContext class that works as unit of work for data entities

---
+++ Arvato.IQ.Data/DbStore.cs
@@ -0,0 +1,82 @@
+﻿using Arvato.IQ.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvato.IQ.Data
+{
+    /// <summary>
+    /// The database DbContext that implements IDbStore and works as unit of work for  
+    /// </summary>
+    public partial class DbStore : DbContext, IDbStore
+    {
+        /// <summary>
+        /// the default constructor
+        /// </summary>
+        public DbStore() : this("name=DbStoreConnection")
+        {
+        }
+
+        /// <summary>
+        /// constructor accept connection string
+        /// </summary>
+        /// <param name="nameOrConnectionString">the database connection string </param>
+        public DbStore(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        /// <summary>
+        /// Building up Database Configuration
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.AddFromAssembly(typeof(DbStore).Assembly);
+        }
+
+        /// <summary>
+        /// Dispose DbStore
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+
+#if DEBUG
+            Debug.WriteLine("DbStore : Disposing .... ");
+#endif
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Save changes Asynchronous
+        /// </summary>
+        /// <returns></returns>
+        public override Task<int> SaveChangesAsync()
+        {
+            // any additional processing before saving will occure here  
+            return base.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Create new instance of DbStore
+        /// </summary>
+        /// <returns></returns>
+        public static DbStore Create()
+        {
+            var db = new DbStore();
+            db.Database.Initialize(true);
+            return db;
+        }
+
+       
+    }
+
+}
